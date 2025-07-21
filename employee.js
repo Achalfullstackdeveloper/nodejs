4ffb2f2d75ee8b8e01112e446105a64a0dac9105
Achalfullstackdeveloper <160086739+Achalfullstackdeveloper@users.noreply.github.com>
2025-07-21 22:16:10 +0530

    Add files via upload

---
+++ employee.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+let studentData = [];
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/students", (req, res) => {
+  res.send(studentData);
+});
+app.post("/student", (req, res) => {
+  // let stringData = "";
+  // req.on("data", (data) => {
+  //   stringData += data;
+  // });
+  //WHEN DATA READING IS COMPLETE
+  // req.on("end", () => {
+  //   studentData.push= JSON.parse(stringData);
+  //   res.end("Message posted successfully");
+  // });
+
+  const parsedData = req.body;
+  studentData.push(parsedData);
+  res.end("message received");
+});
+ app.get("/",(req,res) => {
+    res.end("page not found")
+ });
+ const server = http.createServer(app);
+  server.listen(4200, () => {
+    console.log("Server is running at http://localhost:4200/");
+  });
