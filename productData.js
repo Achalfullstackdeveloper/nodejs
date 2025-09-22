4c1c4544e3021a36395cc019e78b52015dbfb4e8
Achalfullstackdeveloper <160086739+Achalfullstackdeveloper@users.noreply.github.com>
2025-09-22 21:58:25 +0530

    Add files via upload

---
+++ productData.js
@@ -0,0 +1,37 @@
+productData = [
+    {
+        name: "laptop",
+        price: 45000,
+        image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._SL1500_.jpg'
+    },
+    {
+        name: "smart Watch",
+        price: 2000,
+        image: 'https://m.media-amazon.com/images/I/61w8Z5g5ZXL._SL1500_.jpg'
+    },
+    {
+        name: "tablet",
+        price: 25000,
+        image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._SL1500_.jpg'
+    },
+    {
+        name: "Camera",
+        price: 30000,
+        image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._SL1500_.jpg'
+    },
+    {
+        name: "Headphone",
+        price: 5000,
+        image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._SL1500_.jpg'
+    },
+    {
+        name: "Speaker",
+        price: 4000,
+        image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._SL1500_.jpg'
+    },
+    {
+        name: "Monitor",
+        price: 15000,
+        image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._SL1500_.jpg'
+    }
+]
