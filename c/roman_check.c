276a4550accf1178481e3f40f96023d6f6599efd
Achalfullstackdeveloper <160086739+Achalfullstackdeveloper@users.noreply.github.com>
2025-11-08 21:21:19 +0530

    Add files via upload

--- c/roman_check.c
+++ c/roman_check.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+char output[10] = "";
 void numberToRoman(int number, char input[16])
 {
     int values[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     char roman[13][3] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    char output[10] = "";
+    char temp [16]="";
     for (int i = 0; i < 13; i++)
     {
         int value = number / values[i];
         for (int j = 0; j < value; j++)
         {
-            if(strcmp(input, roman[i])==0){
-                  strcat(output, roman[i]);
-        
-            }
-
-          
+            strcat(temp, roman[i]);
         }
         number = number % values[i];
     }
+    printf("\n");
+    if (strcmp(temp, input) == 0)
+    {
+        printf("matched %s\n",temp);
+    }
+    else{
+        printf("not matched %s\n",temp);
+    }
 }
-int a = 10;
+
+
 int main()
 {
     char input[16];
@@ -28,8 +34,8 @@ int main()
     for (int number = 16; number <= 20; number++)
     {
         numberToRoman(number, input);
-        printf("\n");
+        
     }
-    printf("%d",a);
+    printf("\n%s", output);
     return 0;
 }
